12651d313d5228e0512c4684c420227b258ebdb6
stfncrwl999 <stfncrwl378@gmail.com>
2023-08-06 03:51:17 +0300

    add commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap(): Promise<void> {
+  //add some comment
   const app: INestApplication<any> = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
